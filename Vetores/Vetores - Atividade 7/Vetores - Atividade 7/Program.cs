5360d6d64197255b40860210937bb79babf08f97
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-10-27 09:17:36 -0300

    Adicionando Atividades 7 e 8 de vetores

---
+++ Vetores/Vetores - Atividade 7/Vetores - Atividade 7/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Vetores___Atividade_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            int i;
+
+            Console.WriteLine("------------------------------------------");
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite o número para o índice: " + i);
+                numeros[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("------------------------------------------");
+            }
+
+
+            Console.WriteLine("Vetor Final");
+            Console.WriteLine("------------------------------------------");
+            for (i=0; i < 10; i++)
+            {
+                if (numeros[i]%2 == 0)
+                {
+                    Console.Write(numeros[i] +"|");
+                }
+            }
+
+            for (i = 0; i < 10; i++)
+            {
+                if (numeros[i] % 2 != 0)
+                {
+                    Console.Write(numeros[i] + "|");
+                }
+            }
+
+        }
+    }
+}
---
+++ Vetores/Vetores - Atividade 8/Vetores - Atividade 8/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Vetores___Atividade_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numeros = new float[30];
+            int i;
+
+            Console.WriteLine("------------------------------------------");
+
+            for (i = 0; i < 30; i++)
+            {
+                Console.WriteLine("Digite o número para o índice: " + i);
+                numeros[i] = float.Parse(Console.ReadLine());
+                Console.WriteLine("------------------------------------------");
+            }
+
+
+            for (i = 0; i < 30; i++)
+            {
+                if (numeros[i] < 0)
+                {
+                    Console.WriteLine("O índice: " + i + " possui um número negativo");
+                    Console.WriteLine("------------------------------------------");
+                }
+
+            }
+        }
+    }
+}
