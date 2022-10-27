cb2a13567fa65eb638cdbb9fbe03a1c5536330d4
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-10-27 09:21:14 -0300

    Adicionando Atividades 9 e 10 de Vetores

---
+++ Vetores/Vetores - Atividade 10/Vetores - Atividade 10/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Vetores___Atividade_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+            int i;
+
+            Console.WriteLine("------------------------------------------");
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite o número para o índice: " + i);
+                numeros[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("------------------------------------------");
+            }
+            Console.WriteLine("Vetor em Ordem Digitada");
+            Console.WriteLine("-----------------------------------------------");
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write(numeros[i] + "|");
+            }
+            Console.WriteLine("\n-----------------------------------------------");
+            Console.WriteLine("Vetor invertido");
+            Console.WriteLine("-----------------------------------------------");
+
+            Array.Reverse(numeros);
+
+
+
+            for (i=0; i<5; i++)
+            {
+                Console.Write(numeros[i]+"|");
+            }
+        }
+    }
+}
---
+++ Vetores/Vetores - Atividade 9/Vetores - Atividade 9/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Vetores___Atividade_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            int i;
+
+            Console.WriteLine("------------------------------------------");
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite o número para o índice: " + i);
+                numeros[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("------------------------------------------");
+            }
+
+            Array.Sort(numeros);
+
+            for (i=0; i<10; i++)
+            {
+                Console.Write(numeros[i]+"|");
+            }
+
+        }
+    }
+}
