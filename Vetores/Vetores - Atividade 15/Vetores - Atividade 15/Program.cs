81444f05c89b536a558b8abc178e6039a06e5bb8
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-10-28 10:38:08 -0300

    Atividade 15 - Vetores Adicionada

---
+++ Vetores/Vetores - Atividade 15/Vetores - Atividade 15/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Vetores___Atividade_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros1 = new int[20];
+            int[] num2 = new int[20];
+
+            int i;
+
+            for (i=0; i<20; i++)
+            {
+                Console.WriteLine("Digite o valor do índice: "+i);
+                numeros1[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("{ ");
+            for (i=0; i<20;i++) { 
+            Console.WriteLine(numeros1+" ,");
+            }
+            Console.Write(" } \n");
+
+            Array.Reverse(numeros1);
+
+            for (i = 0; i < 20; i++)
+            {
+                num2[i] = numeros1[i];
+            }
+
+            Console.Write("{ ");
+            for (i = 0; i < 20; i++)
+            {
+                Console.WriteLine(num2 + " ,");
+            }
+            Console.Write(" }");
+        }
+    }
+}
