72f2b5086fed63a2bc84b090bba879c4dcc62355
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-06 09:39:28 -0300

    Adicionando Atividade 6 de Matrizes

---
+++ Matrizes/Matriz - Atividade 6/Matriz - Atividade 6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Matriz___Atividade_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gerador = new Random();
+            int[,] numero = new int[4, 4];
+            int i, p;
+
+            for (i=0; i<4; i++)
+            {
+                for (p=0; p<4; p++)
+                {
+                    numero[i,p]= gerador.Next();
+                }
+            }
+
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("Gerador de Números Aleatórios");
+            Console.WriteLine("-----------------------------------------------------------------");
+            for (i=0; i<4; i++)
+            {
+                Console.WriteLine("{ "+numero[i,0]+" ,"+numero[i,1]+" ,"+ numero[i,2]+" ,"+numero[i,3]+" }");
+            }
+
+        }
+    }
+}
