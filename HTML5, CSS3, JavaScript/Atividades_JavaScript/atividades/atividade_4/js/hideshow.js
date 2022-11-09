0f908edfe98bcc01f2e2acac32464d5ad92dea20
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-09 17:08:58 -0300

    Atividade 4 - JavaScript

---
+++ HTML5, CSS3, JavaScript/Atividades_JavaScript/atividades/atividade_4/js/hideshow.js
@@ -0,0 +1,32 @@
+function hideShowTrecho1() {
+   var elemento= document.getElementById('trecho1');
+   if (elemento.style.display == ""){
+    elemento.style.display = "none"
+   }
+   
+   else{
+    elemento.style.display = ""
+   }
+}
+
+function hideShowTrecho2() {
+    var elemento = document.getElementById('trecho2');
+    if (elemento.style.display == ""){
+        elemento.style.display = "none"
+    }
+
+    else{
+        elemento.style.display = ""
+    }
+}
+
+function hideShowTrecho3() {
+    var elemento = document.getElementById('trecho3');
+    if (elemento.style.display == ""){
+        elemento.style.display = "none"
+    }
+
+    else{
+        elemento.style.display = ""
+    }
+}
