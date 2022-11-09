88f91cf740dd31f2ef987addf25c213618cca7ef
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-09 16:35:46 -0300

    Atividades 2 e 3 - JavaScript
    Aqui estão as atividades de JavaScript, nem todas estão prontas ainda, dentro da pasta a um index.html que serve como seletor das atividades contendo a numeração bem como o objetivo da atividade, os HTMLs e JS individuais dos exercicios estão presentes dentro da pasta atividades, também ao uso do Framework Bootstrap (V4.1) para rápida montagem dos HTMLs, ele está intalado de forma local na pasta raiz CSS e JS, dentro da pasta JS raiz também está presente a biblióteca Jquery usada para algumas atividades propostas além de ser um componente necessário dessa versão do Framework. Os Links para todos os HTMLs já estão vínculados, mas não estão todos presentes ainda.

---
+++ HTML5, CSS3, JavaScript/Atividades_JavaScript/atividades/atividade_3/js/alterarCor.js
@@ -0,0 +1,25 @@
+function mudarVerde()
+{
+    document.body.style.background = "#005500";
+}
+
+function mudarAzul()
+{
+    document.body.style.background = "#0004ff";
+}
+
+function mudarRosa()
+{
+    document.body.style.background = "#ff9999";
+}
+
+function mudarVermelho()
+{
+    document.body.style.background = "#ff0000";
+}
+
+function removerCor()
+{
+    document.body.style.background = "";
+}
+
