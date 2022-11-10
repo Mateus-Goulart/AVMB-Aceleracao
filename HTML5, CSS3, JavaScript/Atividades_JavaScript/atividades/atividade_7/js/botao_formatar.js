4e6ab7e565f614037b9f8cd4845a3f6f36950184
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-09 21:32:40 -0300

    Atividades 6 e 7 - JavaScript
    Aqui estão as atividades de JavaScript, nem todas estão prontas ainda, dentro da pasta a um index.html que serve como seletor das atividades contendo a numeração bem como o objetivo da atividade, os HTMLs e JS individuais dos exercicios estão presentes dentro da pasta atividades, também ao uso do Framework Bootstrap (V4.1) para rápida montagem dos HTMLs, ele está intalado de forma local na pasta raiz CSS e JS, dentro da pasta JS raiz também está presente a biblióteca Jquery usada para algumas atividades propostas além de ser um componente necessário dessa versão do Framework. Os Links para todos os HTMLs já estão vínculados, mas não estão todos presentes ainda.

---
+++ HTML5, CSS3, JavaScript/Atividades_JavaScript/atividades/atividade_7/js/botao_formatar.js
@@ -0,0 +1,29 @@
+function aumentarFont(){
+   $("p").css({'fontSize':'40px'})
+}
+
+function diminuirFont(){
+    $("p").css({'fontSize':'5px'})
+}
+
+function normalFont(){
+    $("p").css({'fontSize':'17px'})
+}
+
+function pintarPreto(){
+    $(".texto").css({'background-color':'#000000'})
+}
+
+function pintarBranco(){
+    $(".texto").css({'background-color':'#ffffff'})
+}
+
+function pintarTextoBranco(){
+    $(".texto").css({'color':'#ffffff'})
+}
+
+function pintarTextoPreto(){
+    $(".texto").css({'color':'#000000'})
+}
+
+
