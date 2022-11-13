0914a54b21ce8ab74f198131f8c33227b41c7ac1
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-13 18:13:33 -0300

    Adicionado - Desafio do Jogo da Velha - Estr. de Dados-Lóg. de Programação - Atividades Finalizadas
    Essa atividade tinha como objetivo de criar um jogo da velha pelo console, sendo uma das atividades iniciais e um desafio feito pelo professor (Naquele momento ainda não haviamos trabalhado com matrizes ou vetores).

---
+++ Estr. de Dados-Lóg. de Programação/Jogo da Velha/Jogo da Velha/Program.cs
@@ -0,0 +1,172 @@
+﻿namespace Jogo_da_Velha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[,] matriz = new String[3, 3];
+            int i, p;
+
+            matriz[0, 0] = " "; matriz[0, 1] = " "; matriz[0, 2] = " ";
+            matriz[1, 0] = " "; matriz[1, 1] = " "; matriz[1, 2] = " ";
+            matriz[2, 0] = " "; matriz[2, 1] = " "; matriz[2, 2] = " ";
+
+
+            Console.WriteLine("=================================");
+            Console.WriteLine("Bem-Vindo ao Jogo da Velha");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Se divirta");
+            Console.WriteLine("=================================");
+
+            while (true)
+            {
+
+                Console.WriteLine(" 0 - " + matriz[0, 0] + " | " + matriz[0, 1] + " | " + matriz[0, 2]);
+                Console.WriteLine("     __________");
+                Console.WriteLine(" 1 - " + matriz[1, 0] + " | " + matriz[1, 1] + " | " + matriz[1, 2]);
+                Console.WriteLine("     __________");
+                Console.WriteLine(" 2 - " + matriz[2, 0] + " | " + matriz[2, 1] + " | " + matriz[2, 2]);
+                Console.WriteLine("     0   1   2 ");
+
+
+                Console.WriteLine("=================================");
+                Console.WriteLine("Jogador do X");
+                Console.WriteLine("=================================");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Digite a Linha Para Colocar o X");
+                Console.WriteLine("---------------------------------");
+                i = int.Parse(Console.ReadLine());
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Digite a Coluna Para Colocar o X");
+                Console.WriteLine("---------------------------------");
+                p = int.Parse(Console.ReadLine());
+                matriz[i, p] = "X";
+
+
+                if ((matriz[0, 0] == "X" && matriz[0, 1] == "X" && matriz[0, 2] == "X") ||
+                   (matriz[1, 0] == "X" && matriz[1, 1] == "X" && matriz[1, 2] == "X") ||
+                   (matriz[2, 0] == "X" && matriz[2, 1] == "X" && matriz[2, 2] == "X") ||
+                   (matriz[0, 0] == "X" && matriz[1, 0] == "X" && matriz[2, 0] == "X") ||
+                   (matriz[0, 1] == "X" && matriz[1, 1] == "X" && matriz[2, 1] == "X") ||
+                   (matriz[0, 2] == "X" && matriz[1, 2] == "X" && matriz[2, 2] == "X") ||
+                   (matriz[0, 0] == "X" && matriz[1, 1] == "X" && matriz[2, 2] == "X") ||
+                   (matriz[0, 2] == "X" && matriz[1, 1] == "X" && matriz[2, 0] == "X"))
+                {
+
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("X Ganhou!");
+                    Console.WriteLine("---------------------------------");
+                    break;
+
+                }
+
+                else if ((matriz[0, 0] == "O" && matriz[0, 1] == "O" && matriz[0, 2] == "O") ||
+                   (matriz[1, 0] == "O" && matriz[1, 1] == "O" && matriz[1, 2] == "O") ||
+                   (matriz[2, 0] == "O" && matriz[2, 1] == "O" && matriz[2, 2] == "O") ||
+                   (matriz[0, 0] == "O" && matriz[1, 0] == "O" && matriz[2, 0] == "O") ||
+                   (matriz[0, 1] == "O" && matriz[1, 1] == "O" && matriz[2, 1] == "O") ||
+                   (matriz[0, 2] == "O" && matriz[1, 2] == "O" && matriz[2, 2] == "O") ||
+                   (matriz[0, 0] == "O" && matriz[1, 1] == "O" && matriz[2, 2] == "O") ||
+                   (matriz[0, 2] == "O" && matriz[1, 1] == "O" && matriz[2, 0] == "O"))
+                {
+
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("O Ganhou!");
+                    Console.WriteLine("---------------------------------");
+                    break;
+                }
+                else if ((matriz[0, 0] == " " && matriz[0, 1] == " " && matriz[0, 2] == " ") ||
+                   (matriz[1, 0] == " " && matriz[1, 1] == " " && matriz[1, 2] == " ") ||
+                   (matriz[2, 0] == " " && matriz[2, 1] == " " && matriz[2, 2] == " ") ||
+                   (matriz[0, 0] == " " && matriz[1, 0] == " " && matriz[2, 0] == " ") ||
+                   (matriz[0, 1] == " " && matriz[1, 1] == " " && matriz[2, 1] == " ") ||
+                   (matriz[0, 2] == " " && matriz[1, 2] == " " && matriz[2, 2] == " ") ||
+                   (matriz[0, 0] == " " && matriz[1, 1] == " " && matriz[2, 2] == " ") ||
+                   (matriz[0, 2] == " " && matriz[1, 1] == " " && matriz[2, 0] == " "))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("Ninguém venceu deu velha");
+                    Console.WriteLine("---------------------------------");
+                    continue;
+                }
+
+                Console.WriteLine(" 0 - " + matriz[0, 0] + " | " + matriz[0, 1] + " | " + matriz[0, 2]);
+                Console.WriteLine("     __________");
+                Console.WriteLine(" 1 - " + matriz[1, 0] + " | " + matriz[1, 1] + " | " + matriz[1, 2]);
+                Console.WriteLine("     __________");
+                Console.WriteLine(" 2 - " + matriz[2, 0] + " | " + matriz[2, 1] + " | " + matriz[2, 2]);
+                Console.WriteLine("     0   1   2 ");
+
+                Console.WriteLine("=================================");
+                Console.WriteLine("Jogador do O");
+                Console.WriteLine("=================================");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Digite a Linha Para Colocar o O");
+                Console.WriteLine("---------------------------------");
+                i = int.Parse(Console.ReadLine());
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Digite a Coluna Para Colocar o O");
+                Console.WriteLine("---------------------------------");
+                p = int.Parse(Console.ReadLine());
+                matriz[i, p] = "O";
+
+
+                if ((matriz[0, 0] == "X" && matriz[0, 1] == "X" && matriz[0, 2] == "X") ||
+                   (matriz[1, 0] == "X" && matriz[1, 1] == "X" && matriz[1, 2] == "X") ||
+                   (matriz[2, 0] == "X" && matriz[2, 1] == "X" && matriz[2, 2] == "X") ||
+                   (matriz[0, 0] == "X" && matriz[1, 0] == "X" && matriz[2, 0] == "X") ||
+                   (matriz[0, 1] == "X" && matriz[1, 1] == "X" && matriz[2, 1] == "X") ||
+                   (matriz[0, 2] == "X" && matriz[1, 2] == "X" && matriz[2, 2] == "X") ||
+                   (matriz[0, 0] == "X" && matriz[1, 1] == "X" && matriz[2, 2] == "X") ||
+                   (matriz[0, 2] == "X" && matriz[1, 1] == "X" && matriz[2, 0] == "X"))
+                {
+
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("X Ganhou!");
+                    Console.WriteLine("---------------------------------");
+                    break;
+
+                }
+
+                else if ((matriz[0, 0] == "O" && matriz[0, 1] == "O" && matriz[0, 2] == "O") ||
+                   (matriz[1, 0] == "O" && matriz[1, 1] == "O" && matriz[1, 2] == "O") ||
+                   (matriz[2, 0] == "O" && matriz[2, 1] == "O" && matriz[2, 2] == "O") ||
+                   (matriz[0, 0] == "O" && matriz[1, 0] == "O" && matriz[2, 0] == "O") ||
+                   (matriz[0, 1] == "O" && matriz[1, 1] == "O" && matriz[2, 1] == "O") ||
+                   (matriz[0, 2] == "O" && matriz[1, 2] == "O" && matriz[2, 2] == "O") ||
+                   (matriz[0, 0] == "O" && matriz[1, 1] == "O" && matriz[2, 2] == "O") ||
+                   (matriz[0, 2] == "O" && matriz[1, 1] == "O" && matriz[2, 0] == "O"))
+                {
+
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("O Ganhou!");
+                    Console.WriteLine("---------------------------------");
+                    break;
+                }
+                else if ((matriz[0, 0] == " " && matriz[0, 1] == " " && matriz[0, 2] == " ") ||
+                   (matriz[1, 0] == " " && matriz[1, 1] == " " && matriz[1, 2] == " ") ||
+                   (matriz[2, 0] == " " && matriz[2, 1] == " " && matriz[2, 2] == " ") ||
+                   (matriz[0, 0] == " " && matriz[1, 0] == " " && matriz[2, 0] == " ") ||
+                   (matriz[0, 1] == " " && matriz[1, 1] == " " && matriz[2, 1] == " ") ||
+                   (matriz[0, 2] == " " && matriz[1, 2] == " " && matriz[2, 2] == " ") ||
+                   (matriz[0, 0] == " " && matriz[1, 1] == " " && matriz[2, 2] == " ") ||
+                   (matriz[0, 2] == " " && matriz[1, 1] == " " && matriz[2, 0] == " "))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("Ninguém venceu deu velha");
+                    Console.WriteLine("---------------------------------");
+                    continue;
+                }
+
+            }
+        }
+    }
+}
