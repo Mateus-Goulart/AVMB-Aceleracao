3038c5a622b602ba5770a267ee8a7a7d4610b8d5
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-13 17:55:51 -0300

    Atividades 16,17 de Matrizes - Excercicios Finalizados

---
+++ Matrizes/Matrizes - Atividade 16/Matrizes - Atividade 16/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Matrizes___Atividade_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gerador = new Random();
+            int[,] matriz1 = new int[3, 4];
+            int[,] matriz2 = new int[4,3];
+            int i, p;
+
+            for (i=0; i<3; i++)
+            {
+                for (p=0; p<4; p++)
+                {
+                    matriz1[i, p] = gerador.Next(100);
+                }
+            }
+
+
+            for (i=0; i<4; i++)
+            {
+                for(p=0; p<3; p++)
+                {
+                    matriz2[i, p] = matriz1[p, i];
+                }
+            }
+
+
+            Console.WriteLine("==============================");
+            Console.WriteLine("Matriz 3X4");
+            Console.WriteLine("------------------------------");
+            for (i=0; i<3; i++)
+            {
+                Console.Write("{ ");
+                for (p=0; p<4; p++)
+                {
+                    Console.Write(matriz1[i, p]+" ,");
+                }
+                Console.Write(" } \n");
+            }
+
+            Console.WriteLine("==============================");
+            Console.WriteLine("Matriz 4X3");
+            Console.WriteLine("------------------------------");
+            for (i=0; i<4; i++)
+            {
+                Console.Write("{ ");
+                for (p=0; p<3; p++)
+                {
+                    Console.Write(matriz2[i,p]+" ,");
+                }
+                Console.Write(" } \n");
+            }
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
---
+++ Matrizes/Matrizes - Atividade 17 (Desafio)/Matrizes - Atividade 17 (Desafio)/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Matrizes___Atividade_17__Desafio_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gerador = new Random();
+            int[,] numeros = new int[10, 10];
+            int i, p, min = 0, max = 0;
+
+            for (i=0; i<10; i++)
+            {
+                for (p=0; p<10; p++)
+                {
+                    numeros[i, p] = gerador.Next(100);
+                }
+            }
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Matriz [10,10] Aleatória");
+            Console.WriteLine("--------------------------------------");
+
+            for (i=0; i<10; i++)
+            {
+                Console.Write("{ ");
+                for (p=0; p<10; p++)
+                {
+                    Console.Write(numeros[i, p]+" ,");
+                }
+                Console.Write(" } \n");
+            }
+
+            for (i=0; i<10; i++)
+            {
+                for (p=0; p<10; p++)
+                {
+                    min = numeros[i, 0];
+                    max = numeros[i, 0];
+
+                    if (numeros[i,p] < min)
+                    {
+                        min = numeros[i, p];
+                        
+                    }
+                    if (numeros[i,p] > max)
+                    {
+                        max = numeros[i, p];
+                        
+                    }
+                }
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Número Mínimo da Linha ["+i+"] :"+min);
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Número Maior da Linha ["+i+"] :"+max);
+            }
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+}
