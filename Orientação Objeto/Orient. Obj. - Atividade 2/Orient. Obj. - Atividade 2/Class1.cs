e3c875f8a0b450614c85b612e96f772fce5d4572
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-28 14:00:23 -0300

    Adicionado Atividade 2 de Orientação Objeto

---
+++ Orientação Objeto/Orient. Obj. - Atividade 2/Orient. Obj. - Atividade 2/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orient._Obj.___Atividade_2
+{
+    internal class Televisão
+    {
+        public bool estado = false;
+        public double canal = 10;
+
+        public void ligarTelevisao()
+        {
+            if (estado == false)
+            {
+                Console.WriteLine("A televisão está desligada, a ligando");
+                this.estado = true;
+            }
+            else
+            {
+                Console.WriteLine("A televisão já está ligada");
+            }
+        }
+
+        public void desligarTelevisao()
+        {
+            if (estado == true)
+            {
+                Console.WriteLine("A televisão está ligada, a desligando");
+                this.estado = false;
+            }
+            else
+            {
+                Console.WriteLine("A televisão já está desligada");
+            }
+        }
+
+        public void mudarCanal(double mudanca)
+        {
+            if (estado == true)
+            {
+                Console.WriteLine("Mudando de Canal");
+                Console.WriteLine("---------------------------------------");
+                this.canal = mudanca;
+                Console.WriteLine("Canal Atual: "+this.canal);
+            }
+            else
+            {
+                Console.WriteLine("A televisão está desligada, lige-a primeiro");
+            }
+        }
+    }
+}
---
+++ Orientação Objeto/Orient. Obj. - Atividade 2/Orient. Obj. - Atividade 2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Orient._Obj.___Atividade_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Televisão tv1 = new Televisão();
+            int op;
+            double canal;
+            while (true)
+            {
+                Console.WriteLine("=======================================");
+                Console.WriteLine("O que deseja fazer?");
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("1) Ligar a televisão. \n2) Desligar a televisão. \n3) Mudar de canal");
+                Console.WriteLine("---------------------------------------");
+                op = int.Parse(Console.ReadLine());
+                Console.WriteLine("---------------------------------------");
+                if (op == 1)
+                {
+                    tv1.ligarTelevisao();
+                }
+                else if (op == 2)
+                {
+                    tv1.desligarTelevisao();
+
+                }
+                else if (op == 3)
+                {
+                    Console.WriteLine("Digite o canal que deseja assistir:");
+                    canal = double.Parse(Console.ReadLine());
+                    tv1.mudarCanal(canal);
+                }
+                else
+                {
+                    Console.WriteLine("Opção Ínvalida");
+                }
+            }
+        }
+    }
+}
