4a0cce609d8569467db6cc976b0e9458af5b46c2
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-28 13:36:22 -0300

    Adicionado Atividade 1 - Orientação Objeto

---
+++ Orientação Objeto/Orient. Obj. - Atividade 1/Orient. Obj. - Atividade 1/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orient._Obj.___Atividade_1
+{
+    internal class Livro
+    {
+        public string titulo="", autor="", resumo = "", data_lancamento = "", editora = "";
+        public double numero_paginas=0;
+
+        public void cadastrarLivro()
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("Insira o título do livro: ");
+            Console.WriteLine("--------------------------");
+            this.titulo = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira o autor: ");
+            Console.WriteLine("--------------------------");
+            this.autor = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira o resumo do Livro: ");
+            Console.WriteLine("--------------------------");
+            this.resumo = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira a data de lançamento: ");
+            Console.WriteLine("--------------------------");
+            this.data_lancamento = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira a editora do livro: ");
+            Console.WriteLine("--------------------------");
+            this.editora = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira número de páginas do livro: ");
+            Console.WriteLine("--------------------------");
+            this.numero_paginas = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------");
+        }
+
+        public void exibirLivro()
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("Título: "+this.titulo);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Autor: " + this.autor);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Resumo: " + this.resumo);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Editora: " + this.editora);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Data de Lançamento: " + this.data_lancamento);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Número de Páginas do Livro: " + this.numero_paginas);
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
---
+++ Orientação Objeto/Orient. Obj. - Atividade 1/Orient. Obj. - Atividade 1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Orient._Obj.___Atividade_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op;
+            Livro l1= new Livro();
+            while (true)
+            {
+                Console.WriteLine("==========================================");
+                Console.WriteLine("Deseja adicionar ou visualizar um Livro:");
+                Console.WriteLine("------------------------------------------");
+                Console.Write("1) Para Cadastrar um Livro. \n2) Para visualizar o livro.\n");
+                Console.WriteLine("------------------------------------------");
+                op = int.Parse(Console.ReadLine());
+                Console.WriteLine("------------------------------------------");
+                if (op == 1)
+                {
+                    l1.cadastrarLivro();
+                }
+                else if (op == 2)
+                {
+                    l1.exibirLivro();
+                }
+                else
+                {
+                    Console.WriteLine("Opção Ínvalida");
+                }
+            }
+        }
+    }
+}
