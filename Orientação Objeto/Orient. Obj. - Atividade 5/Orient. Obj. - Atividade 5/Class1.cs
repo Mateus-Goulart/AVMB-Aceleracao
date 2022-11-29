d1c91a267ccec38adf9b7bfd1fd8647343fc1fef
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-29 14:36:15 -0300

    Adicionado Atividade 5 - Orientação Objeto

---
+++ Orientação Objeto/Orient. Obj. - Atividade 5/Orient. Obj. - Atividade 5/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orient._Obj.___Atividade_5
+{
+    internal class Livro
+    {
+        public string titulo = "", autor = "", resumo = "", data_lancamento = "", editora = "";
+        public double numero_paginas = 0;
+
+        public void cadastrarLivro()
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("Insira o título do livro: ");
+            Console.WriteLine("--------------------------");
+            this.titulo = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira o autor: ");
+            Console.WriteLine("--------------------------");
+            this.autor = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira o resumo do Livro: ");
+            Console.WriteLine("--------------------------");
+            this.resumo = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira a data de lançamento: ");
+            Console.WriteLine("--------------------------");
+            this.data_lancamento = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira a editora do livro: ");
+            Console.WriteLine("--------------------------");
+            this.editora = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Insira número de páginas do livro: ");
+            Console.WriteLine("--------------------------");
+            this.numero_paginas = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------");
+        }
+
+        public void ConstrCadastrarLivro(string titulo, string autor, string data_lancamento, string editora, int numero_paginas)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            Console.WriteLine("Digite o Resumo do livro a ser cadastrado");
+            Console.WriteLine("--------------------------");
+            this.resumo = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            this.data_lancamento = data_lancamento;
+            this.editora = editora;
+            this.numero_paginas = numero_paginas;
+        }
+
+        public void exibirLivro()
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("Título: " + this.titulo);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Autor: " + this.autor);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Resumo: " + this.resumo);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Editora: " + this.editora);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Data de Lançamento: " + this.data_lancamento);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Número de Páginas do Livro: " + this.numero_paginas);
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
---
+++ Orientação Objeto/Orient. Obj. - Atividade 5/Orient. Obj. - Atividade 5/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Orient._Obj.___Atividade_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string titulo = "", autor = "", data_lancamento = "", editora = "";
+            int numero_paginas = 0, op;
+            while (true)
+            {
+                Livro l1 = new Livro();
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("Deseja Cadastrar/Exibir um livro");
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("1 - Para Cadastrar um Livro | 2 - Para Cadastrar um Livro (Construtor) | 3 - Exibir Livro");
+                Console.WriteLine("--------------------------");
+                op = int.Parse(Console.ReadLine());
+                Console.WriteLine("--------------------------");
+                if (op == 1)
+                {
+                    l1.cadastrarLivro();
+                }
+                else if (op == 2)
+                {
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine("Digite o Título do Livro:");
+                    Console.WriteLine("--------------------------");
+                    titulo = Console.ReadLine();
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine("Digite o Autor:");
+                    Console.WriteLine("--------------------------");
+                    autor = Console.ReadLine();
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine("Digite a Data de Lançamento:");
+                    Console.WriteLine("--------------------------");
+                    data_lancamento = Console.ReadLine();
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine("Digite a Editora:");
+                    Console.WriteLine("--------------------------");
+                    editora = Console.ReadLine();
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine("Digite o Número de Páginas do Livro");
+                    Console.WriteLine("--------------------------");
+                    numero_paginas = int.Parse(Console.ReadLine());
+                    Console.WriteLine("--------------------------");
+                    l1.ConstrCadastrarLivro(titulo, autor, data_lancamento, editora, numero_paginas);
+                }
+                else if (op == 3)
+                {
+                    l1.exibirLivro();
+                }
+                else
+                {
+                    Console.WriteLine("Opção Ínvalida");
+                    Console.WriteLine("--------------------------");
+                }
+            }
+        }
+    }
+}
