07517894eae07669c1f584fb1708bab594546843
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-29 16:26:53 -0300

    Adicionado Atividade 8 - Orientação Objeto

---
+++ Orientação Objeto/Orient. Obj. - Atividade 8/Orient. Obj. - Atividade 8/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orient._Obj.___Atividade_8
+{
+    internal class Funcionario
+    {
+        private int rg=0;
+        private string nome="", cpf="", data_nascimento = "", endereco = "";
+
+        public void cadastarFuncionario(string nome, string cpf, int rg, string data_nascimento, string endereco)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.rg = rg;
+            this.data_nascimento = data_nascimento;
+            this.endereco = endereco;
+        }
+
+        public void exibirFicha()
+        {
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Nome: "+this.nome);
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("CPF: "+this.cpf);
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("RG: " + this.rg);
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Data de Nascimento: " + this.data_nascimento);
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Endereço: " + this.endereco);
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+}
---
+++ Orientação Objeto/Orient. Obj. - Atividade 8/Orient. Obj. - Atividade 8/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Orient._Obj.___Atividade_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, cpf, data_nascimento, endereco;
+            int rg, op;
+            Funcionario f1 = new Funcionario();
+            while (true)
+            {
+                Console.WriteLine("=============================================");
+                Console.Write("1) Para Cadastrar o Funcionario. \n2) Para ver a ficha do funcionario. \n");
+                Console.WriteLine("---------------------------------------------");
+                op = int.Parse(Console.ReadLine());
+                Console.WriteLine("---------------------------------------------");
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o nome do Funcionario: ");
+                        Console.WriteLine("---------------------------------------------");
+                        nome = Console.ReadLine();
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine("Digite o CPF do Funcionario: ");
+                        Console.WriteLine("---------------------------------------------");
+                        cpf = Console.ReadLine();
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine("Digite o RG do Funcionario: ");
+                        Console.WriteLine("---------------------------------------------");
+                        rg = int.Parse(Console.ReadLine());
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine("Digite a Data de Nascimento do Funcionario:");
+                        Console.WriteLine("---------------------------------------------");
+                        data_nascimento = Console.ReadLine();
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine("Digite o Endereço do Funcionario: ");
+                        Console.WriteLine("---------------------------------------------");
+                        endereco = Console.ReadLine();
+                        Console.WriteLine("---------------------------------------------");
+                        f1.cadastarFuncionario(nome,cpf,rg,data_nascimento,endereco);
+                        break;
+                    case 2:
+                        f1.exibirFicha();
+                        break;
+                    default:
+                        Console.WriteLine("Opção Ínvalida.");
+                        Console.WriteLine("---------------------------------------------");
+                        break;
+                }
+            }
+        }
+    }
+}
