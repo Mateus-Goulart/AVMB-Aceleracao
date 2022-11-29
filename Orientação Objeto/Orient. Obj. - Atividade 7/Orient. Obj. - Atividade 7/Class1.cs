b1141331d7682a20ce126dd386d9c042ef60505c
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-29 15:56:18 -0300

    Adicionado Atividade 7 - Orientação Objeto

---
+++ Orientação Objeto/Orient. Obj. - Atividade 7/Orient. Obj. - Atividade 7/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orient._Obj.___Atividade_7
+{
+    internal class Personagem
+    {
+        public string nome = "";
+        public int posicao = 0;
+        public string[] itens = new string[20];
+
+        public void mudarPosição(int posicao)
+        {
+            this.posicao = posicao;
+        }
+
+        public void coletarItens(int ItensColetados)
+        {
+            Console.WriteLine("=================================");
+            for (int i = 0; i < ItensColetados; i++)
+            {
+                Console.WriteLine("Qual Iten foi coletado?");
+                Console.WriteLine("----------------------------------");
+                itens[i] = Console.ReadLine();
+                Console.WriteLine("----------------------------------");
+            }
+            Console.WriteLine("=================================");
+
+        }
+
+        public void exibirInventario()
+        {
+            Console.WriteLine("=================================");
+            for (int i = 0; i < itens.Length; i++)
+            {
+                Console.WriteLine("Slot " + i + ": " + itens[i]);
+                Console.WriteLine("----------------------------------");
+            }
+            Console.WriteLine("=================================");
+        }
+
+        public void andar(int direcao)
+        {
+            while (true)
+            {
+                int op = direcao;
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("Você andou para Frente.");
+                        continue;
+                    case 2:
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("Você andou para Esquerda.");
+                        break;
+                    case 3:
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("Você andou para Direita.");
+                        continue;
+                    case 4:
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("Você andou para Para Trás.");
+                        continue;
+                    case 5:
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("Você Parou.");
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
+    }
+}
---
+++ Orientação Objeto/Orient. Obj. - Atividade 7/Orient. Obj. - Atividade 7/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Orient._Obj.___Atividade_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem p1 = new Personagem();
+            int posicao, op, direcao;
+            Console.WriteLine("==================================");
+            Console.WriteLine("Bem-Vindo, Por favor se apresente:");
+            Console.WriteLine("----------------------------------");
+            p1.nome = Console.ReadLine();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Seja Bem-Vindo, " + p1.nome);
+            Console.WriteLine("----------------------------------");
+            while (true)
+            {
+                Console.WriteLine("O que deseja fazer agora?");
+                Console.WriteLine("----------------------------------");
+                Console.Write("1) Andar para alguma direção. \n" +
+                    "2) Coletar itens. (Diga Quantos itens você coletou, seu inventário só possui 20 Slots) \n" +
+                    "3) Abrir Inventário \n" +
+                    "4) Mudar de Posição (em caso de estar em grupo). \n" +
+                    "5) Caso deseje sair. \n");
+                Console.WriteLine("----------------------------------");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("=================================");
+                        Console.WriteLine("Para onde deseja ir?");
+                        Console.WriteLine("----------------------------------");
+                        Console.Write("1) Para a Frete. \n 2) Para a Esquerda. \n 3) Para Direita. \n 4) Para Trás. \n 5) Deseja parar. \n");
+                        direcao = int.Parse(Console.ReadLine());
+                        p1.andar(direcao);
+                        break;
+                    case 2:
+                        Console.WriteLine("Quantos itens você coletou?");
+                        int number = int.Parse(Console.ReadLine());
+                        p1.coletarItens(number);
+                        break;
+                    case 3:
+                        p1.exibirInventario();
+                        break;
+                    case 4:
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("Qual a posição desejada?");
+                        Console.WriteLine("----------------------------------");
+                        posicao = int.Parse(Console.ReadLine());
+                        Console.WriteLine("==================================");
+                        p1.mudarPosição(posicao);
+                        break;
+                    case 5:
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opção Ínvalida.");
+                        break;
+                }
+            }
+        }
+    }
+}
