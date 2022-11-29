2cc057df01c55d537a187145a511d5573b225dfd
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-29 14:03:16 -0300

    Adicionado Atividade 4 - Orientação Objeto

---
+++ Orientação Objeto/Orient. Obj. - Atividade 4/Orient. Obj. - Atividade 4/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orient._Obj.___Atividade_4
+{
+    internal class Pessoa
+    {
+        public string nome = "", cpf = "", data_nascimento = "";
+        public int rg=0;
+
+        public void cadastrarPessoa()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Digite o nome da pessoa a ser cadastrada:");
+            Console.WriteLine("-----------------------------------------");
+            this.nome = Console.ReadLine();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Digite o CPF da pessoa a ser cadastrada:");
+            Console.WriteLine("-----------------------------------------");
+            this.cpf = Console.ReadLine();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Digite a data de nascimento da pessoa a ser cadastrada");
+            Console.WriteLine("-----------------------------------------");
+            this.data_nascimento = Console.ReadLine();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Digite o RG da pessoa a ser cadastrada");
+            Console.WriteLine("-----------------------------------------");
+            this.rg = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------------------");
+        }
+        public void exibirCadastro()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Nome: "+this.nome);
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("CPF: "+this.cpf);
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Data de Nascimento: "+this.data_nascimento);
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("RG: "+this.rg);
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
---
+++ Orientação Objeto/Orient. Obj. - Atividade 4/Orient. Obj. - Atividade 4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Orient._Obj.___Atividade_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            while (true)
+            {
+                Console.WriteLine("Cadastrar ou Exibir pessoa cadastrada:");
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("1 - Cadastrar | 2 - Exibir Cadastro");
+                Console.WriteLine("-----------------------------------------");
+                int op = int.Parse(Console.ReadLine());
+                Console.WriteLine("-----------------------------------------");
+                if (op == 1)
+                {
+                    p1.cadastrarPessoa();
+                }
+                else if (op == 2)
+                {
+                    p1.exibirCadastro();
+                }
+                else
+                {
+                    Console.WriteLine("Opção Ínvalida.");
+                    Console.WriteLine("-----------------------------------------");
+                }
+            }
+        }
+    }
+}
