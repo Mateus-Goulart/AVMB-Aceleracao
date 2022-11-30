314dd62e1d0185977fce75eb04d82f4481e21622
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-11-30 12:57:16 -0300

    Adicionado Atividade 9 - Orientação Objeto

---
+++ Orientação Objeto/Orient. Obj. - Atividade 9/Orient. Obj. - Atividade 9/Class1.cs
@@ -0,0 +1,37 @@
+﻿namespace Orient._Obj.___Atividade_9
+{
+    internal class Motor
+    {
+        private string nomeFabricante = "";
+        private int potencia = 0;
+        private string tipo = "";
+
+        public void cadastrarMotor()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Insira o nome do fabricante:");
+            Console.WriteLine("-----------------------------------------");
+            this.nomeFabricante = Console.ReadLine();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Insira a potencia do motor:");
+            Console.WriteLine("-----------------------------------------");
+            this.potencia = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Insira o tipo do motor:");
+            Console.WriteLine("-----------------------------------------");
+            this.tipo = Console.ReadLine();
+            Console.WriteLine("-----------------------------------------");
+        }
+
+        public void exibirMotor()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Nome do Fabricante: " + this.nomeFabricante);
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Potencia: "+this.potencia);
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Tipo: "+this.tipo);
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
---
+++ Orientação Objeto/Orient. Obj. - Atividade 9/Orient. Obj. - Atividade 9/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Orient._Obj.___Atividade_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Motor m1 = new Motor();
+            int op;
+
+
+            while (true)
+            {
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("1) Para Cadastrar o motor | 2) Para Visualizar o motor");
+                Console.WriteLine("-----------------------------------------");
+                op = int.Parse(Console.ReadLine());
+                Console.WriteLine("-----------------------------------------");
+                switch (op)
+                {
+                    case 1:
+                        m1.cadastrarMotor();
+                        break;
+                    case 2:
+                        m1.exibirMotor();
+                        break;
+                    default:
+                        Console.WriteLine("Opção Ínvalida");
+                        Console.WriteLine("-----------------------------------------");
+                        break;
+                }
+            }
+        }
+    }
+}
