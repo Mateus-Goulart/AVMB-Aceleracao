24772c0977935a17219c2bbc20a2479dc9507b0b
Mateus-Goulart <111802512+Mateus-Goulart@users.noreply.github.com>
2022-12-13 14:58:19 -0300

    Atividades de HTML, CSS, JavaScript - Finalizadas
    Adicionado Atividade 9, 10 de JavaScript e o Projeto de HTML, CSS, JavaScript

---
+++ HTML5, CSS3, JavaScript/Atividade - Projeto - HTML, CSS, JavaScript/Personalizado/js/apresentacao.js
@@ -0,0 +1,24 @@
+
+$("#mostroario1").click(function(){
+    $("#apresentacao").html('<img src="img/mostroario1.jpg" width="97%" height="100%" class="b-radius1 m-left">')
+    $(".div-mostroario").removeClass("selected");
+    $(this).addClass("selected");
+});
+
+$("#mostroario2").click(function(){
+    $("#apresentacao").html('<img src="img/mostroario2.jpg" width="97%" height="100%" class="b-radius1 m-left">')
+    $(".div-mostroario").removeClass("selected");
+    $(this).addClass("selected");
+});
+
+$("#mostroario3").click(function(){
+    $("#apresentacao").html('<img src="img/mostroario3.jpg" width="97%" height="100%" class="b-radius1 m-left">')
+    $(".div-mostroario").removeClass("selected");
+    $(this).addClass("selected");
+});
+
+$("#mostroario4").click(function(){
+    $("#apresentacao").html('<img src="img/mostroario4.jpg" width="97%" height="100%" class="b-radius1 m-left">')
+    $(".div-mostroario").removeClass("selected");
+    $(this).addClass("selected");
+});
